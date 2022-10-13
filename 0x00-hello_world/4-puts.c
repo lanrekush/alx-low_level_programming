719a79e04274b4f6b4872c17fe5b30fc9626fe28
Lanre Kushalu <lanrekushalu@gmail.com>
2022-10-13 15:16:00 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
-/* using put */
+/**
+ * main - entry point
+ *
+ * Return: must be 0
+ */
 int  main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
