520d5d58371736761bf30513274a27a3acd69867
Lanre Kushalu <lanrekushalu@gmail.com>
2022-10-17 20:55:36 -0700

    3-print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,12 @@ int main(void)
 {
 	int x;
 
-	for (x = 'A'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
 	putchar('\n');
 	return (0);
 }
