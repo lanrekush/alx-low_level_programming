b5929762522937e05389e7fef8b77fc44d94b717
Lanre Kushalu <lanrekushalu@gmail.com>
2022-10-17 21:36:04 -0700

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10
+ *
+ * Return: 0 value
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+
+	printf("\n");
+	return (0);
+}
