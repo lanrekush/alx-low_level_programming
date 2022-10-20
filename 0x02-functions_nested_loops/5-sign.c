2b6c8143c44e51f5391ea150741233b503a2c0b8
Lanre Kushalu <lanrekushalu@gmail.com>
2022-10-20 08:33:25 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,23 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
 		_putchar('\n');
+	}
 
 	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
 		_putchar('\n');
+	}
 
 	else
+	{
 		_putchar('-1');
 		return ('-1');
 		_putchar('\n'0;
+	}
 }
