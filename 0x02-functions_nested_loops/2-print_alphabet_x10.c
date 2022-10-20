07550a81512a8c8e123c77aa9b1290c3e7dd4c2d
Lanre Kushalu <lanrekushalu@gmail.com>
2022-10-19 23:24:37 -0700

    2-print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,12 @@
 void print_alphabet_x10(void)
 {
 	char alp;
-	int x;
+	int x = 0;
 
 	while (x < 10)
 	{
 		for (alp = 'a'; alp <= 'z'; alp++)
-			{	_putchar(alp);
-			}
+			_putchar(alp);
 		_putchar('\n');
 		x++;
 	}
