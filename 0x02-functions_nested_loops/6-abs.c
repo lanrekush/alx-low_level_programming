5fe8eaa6a2e77b15404af8f6c4df454bd95f6256
Lanre Kushalu <lanrekushalu@gmail.com>
2022-10-20 09:18:31 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n:is the value
+ * Return: the value n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,6 +15,7 @@
   int _isalpha(int c);
   int _islower(int c);
   int print_sign(int n);
+  int _abs(int);
 
 
 
